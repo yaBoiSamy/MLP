90378bd7584584eef2faacb59b79dd11a0d60458
Samuel Bélanger <samprogrammation@gmail.com>
2024-10-06 23:31:16 -0400

    start of backprop

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ class NeuralNetwork:
 
     def forward(self, inputs):
         sigmoid = lambda x: 1 / (1 + np.exp(-x))
-        currentLayer = inputs
+        self.activation = np.array([inputs])
         for weights, biases in zip(self.weights, self.biases):
-            currentLayer = sigmoid(np.dot(weights, currentLayer) + biases)
-        return currentLayer
+            np.append(self.activation, sigmoid(np.dot(weights, self.activation[-1]) + biases), axis=0)
+        return self.activation[-1]
 
-    def backward(self):
+    def backward(self, inputs, tags):
         sigmoid_prime = lambda x: x * (1 - x)
+        output_error = (self.forward(inputs) - np.array(tags))*sigmoid_prime()
+
+
+
+
 
