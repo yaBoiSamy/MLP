c5053326f70be4b98efe677eba471cd80ec7892a
Samuel Bélanger <samprogrammation@gmail.com>
2024-10-08 00:44:39 -0400

    fully functional mlp

--- main.py
+++ main.py
@@ -9,33 +9,35 @@ class NeuralNetwork:
 
     def forward(self, inputs):
         sigmoid = lambda x: 1 / (1 + np.exp(-x))
-        self.activation = [np.array(inputs)]
+        self.activations = [np.array(inputs)]
         for weights, biases in zip(self.weights, self.biases):
-            self.activation.append(sigmoid(np.dot(weights, self.activation[-1]) + biases))
-        return self.activation[-1]
+            self.activations.append(sigmoid(np.dot(weights, self.activations[-1]) + biases))
+        return self.activations[-1]
 
     def backward(self, inputs, tags, growthSpeed=1):
         sigmoid_prime = lambda x: x * (1 - x)
         output = self.forward(inputs)
-        Z = [(np.array(tags) - output) * sigmoid_prime(output)]
-        for layer, weights in enumerate(self.weights[:1:-1], 2):
-            Z.append(np.dot(weights.T, Z[-1])*sigmoid_prime(self.activation[-layer]))
-        Z = Z[::-1]
-        for layer in range(len(Z)):
-            self.weights[layer] += np.outer(Z[layer], self.activation[layer])*growthSpeed
-            self.biases[layer] += Z[layer]*growthSpeed
 
+        Z = [(np.array(tags) - output) * sigmoid_prime(output)]
+        for layer in range(len(self.weights) - 1, 0, -1):
+            Z.append(np.dot(self.weights[layer].T, Z[-1]) * sigmoid_prime(self.activations[layer]))
+        Z = Z[::-1]
+
+        for layer in range(len(Z)):
+            self.weights[layer] += growthSpeed * np.outer(Z[layer], self.activations[layer])
+            self.biases[layer] += growthSpeed * Z[layer]
 
 
 speed = 1
 XOR = NeuralNetwork(2, [2, 1])
-print(XOR.weights)
-for i in range(100):
+
+for i in range(10000):
     XOR.backward([0,0], 0, speed)
     XOR.backward([0,1], 1, speed)
     XOR.backward([1,0], 1, speed)
     XOR.backward([1,1], 0, speed)
-print(XOR.weights)
+
+print("training complete")
 while True:
     print(XOR.forward(list(map(int, input().split()))))
 
